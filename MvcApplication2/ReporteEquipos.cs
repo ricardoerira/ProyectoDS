767cb1de49d62515dd73ea30ccf5a5b6f753b8c0
ricardoerira <ricardoerira@gmail.com>
2015-09-16 20:33:42 -0500

    ajustes modelo

--- MvcApplication2/ReporteEquipos.cs
+++ MvcApplication2/ReporteEquipos.cs
@@ -111,6 +111,14 @@ namespace MvcApplication2 {
                 return this.DataDefinition.ParameterFields[2];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_fecha {
+            get {
+                return this.DataDefinition.ParameterFields[3];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
